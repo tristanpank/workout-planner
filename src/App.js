6da5d2a86f20a32b3f802fc29dc6be820b2109a8
tristanpank <tristanpank@gmail.com>
2023-02-12 11:01:25 -0500

    Implement basic signIn/signOut Abstracted auth functionality into auth.js file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import { useState } from 'react';
-import { auth } from "./firebase.config";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
-const provider = new GoogleAuthProvider();
+import { signIn, signOutUser } from "./firebase/auth";
 
 
 function App() {
-  const [currUser, setCurrUser] = useState();
-  function handleClick(e) {
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      setCurrUser(user.displayName);
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-  } 
+  const [currUser, setCurrUser] = useState({displayName: ""});
+  
+
   return(
     <div>
-      <button onClick={handleClick}>Sign In</button>
-      <div>{currUser}</div>
+      <button onClick={() => signIn(setCurrUser)}>Sign In</button>
+      <button onClick={() => signOutUser(setCurrUser)}>Sign Out</button>
+      <div>{currUser.displayName}</div>
     </div>
   )
 }
