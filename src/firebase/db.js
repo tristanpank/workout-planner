ec2852037d3f5397f06722f343e75ae4e0352e9b
tristanpank <tristanpank@gmail.com>
2023-02-13 19:09:12 -0500

    Create data hierarchy if user doesn't exist

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,11 +1,38 @@
 import { db } from "./firebase.config";
-import { doc, setDoc} from "firebase/firestore";
+import { doc, setDoc, collection, getDoc } from "firebase/firestore";
 
 
 // Sets user variables whenever a user is logged in
 export async function setUserDB(user) {
-  await setDoc(doc(db, "users", user.uid), {
-    name: user.displayName,
-    email: user.email,
-  })
-}
+  const userRef = await getDoc(doc(db, "users", user.uid));
+  if (userRef.exists()) {
+    console.log("exists");
+  } else {
+    await setDoc(doc(db, "users", user.uid), {
+      name: user.displayName,
+      email: user.email,
+    });
+    let today = new Date().toISOString().slice(0, 10)
+    await setDoc(doc(db, "users", user.uid, "days", today), {
+      date: today,
+      workouts: {},
+    });
+    await setDoc(doc(db, "users", user.uid, "workouts", "run"), {
+      workouts: [],
+    });
+    await setDoc(doc(db, "users", user.uid, "workouts", "bike"), {
+      workouts: [],
+    });
+    await setDoc(doc(db, "users", user.uid, "workouts", "swim"), {
+      workouts: [],
+    });
+    
+  }
+  // await setDoc(doc(db, "users", user.uid), {
+  //   name: user.displayName,
+  //   email: user.email,
+  // })
+  // await setDoc(doc(db, "users", user.uid, "days", "day"), {
+  //   day: "testday"
+  // });
+};
